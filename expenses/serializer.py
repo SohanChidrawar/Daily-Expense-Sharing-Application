f55e6b5c68905f738badb39a29ca5c57dca1e989
Sohan Chidrawar <99244757+SohanChidrawar@users.noreply.github.com>
2024-07-30 14:47:27 +0530

    Update serializer.py

--- expenses/serializer.py
+++ expenses/serializer.py
@@ -1,51 +1,13 @@
-# from rest_framework import serializers
-# from .models import User, Expense, ExpenseParticipant
-
-# class UserSerializer(serializers.ModelSerializer):
-#     class Meta:
-#         model = User
-#         # Fields to be serialized for User
-#         fields = ['id', 'email', 'name', 'mobile_number']
-
-# class ExpenseParticipantSerializer(serializers.ModelSerializer):
-#     class Meta:
-#         model = ExpenseParticipant
-#         # Fields to be serialized for Expense Participant
-#         fields = ['user', 'amount', 'percentage']
-
-# class ExpenseSerializer(serializers.ModelSerializer):
-#     participants = ExpenseParticipantSerializer(many=True)
-
-#     class Meta:
-#         model = Expense
-#         # field to serialized for expenses
-#         fields = ['id', 'description', 'total_amount', 'expense_type', 'created_by', 'participants']
-
-#     def validate(self, data):
-#         # Validate that the total percentage for percentage splits adds up to 100
-#         if data['expense_type'] == 'percentage':
-#             total_percentage = sum(participant['percentage'] for participant in data['participants'])
-#             if total_percentage != 100:
-#                 raise serializers.ValidationError("Total percentage must equal 100.")
-#         return data
-
-#     def create(self, validated_data):
-#         # Create an Expense instance and associated ExpenseParticipants
-#         participants_data = validated_data.pop('participants')
-#         expense = Expense.objects.create(**validated_data)
-
-#         for participant_data in participants_data:
-#             ExpenseParticipant.objects.create(expense=expense, **participant_data)
-#         return expense
-
 from rest_framework import serializers
 from .models import User, Expense, ExpenseParticipant
 
+# Serializer for User model
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ['id', 'email', 'name', 'mobile_number']
 
+# Serializer for ExpenseParticipant model
 class ExpenseParticipantSerializer(serializers.ModelSerializer):
     User = serializers.PrimaryKeyRelatedField(queryset=User.objects.all())
 
@@ -53,6 +15,7 @@ class ExpenseParticipantSerializer(serializers.ModelSerializer):
         model = ExpenseParticipant
         fields = ['user', 'amount', 'percentage','User']
 
+# Serializer for Expense model
 class ExpenseSerializer(serializers.ModelSerializer):
     participants = ExpenseParticipantSerializer(many=True)
 
@@ -67,10 +30,11 @@ class ExpenseSerializer(serializers.ModelSerializer):
                 raise serializers.ValidationError("Total percentage must equal 100.")
         return data
 
+    # Overriding the create method to handle nested data
     def create(self, validated_data):
-        participants_data = validated_data.pop('participants')
-        expense = Expense.objects.create(**validated_data)
+        participants_data = validated_data.pop('participants')        # Extracting participants data
+        expense = Expense.objects.create(**validated_data)            # Creating Expense instance
         for participant_data in participants_data:
-            ExpenseParticipant.objects.create(expense=expense, **participant_data)
+            ExpenseParticipant.objects.create(expense=expense, **participant_data)      # Creating ExpenseParticipant instances
         return expense
 
