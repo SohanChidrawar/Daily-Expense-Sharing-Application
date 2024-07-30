0a05ef51e038359aa1aee5f47ec713e124e13ce6
Sohan Chidrawar <99244757+SohanChidrawar@users.noreply.github.com>
2024-07-30 14:47:10 +0530

    Update views.py

--- expenses/views.py
+++ expenses/views.py
@@ -1,16 +1,21 @@
-#from django.shortcuts import render
-
-#from rest_framework import serializers
+from rest_framework import generics
 from rest_framework import viewsets
 from .models import User, Expense
 from .serializer import UserSerializer, ExpenseSerializer
 from django.http import HttpResponse
 import csv
 
+# API view to create and list users
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
 
+# API view to retrieve user details
+class UserDetailView(generics.RetrieveAPIView):
+    queryset = User.objects.all()  # Queryset to get user details
+    serializer_class = UserSerializer  # Serializer to use for this view
+
+# API view to create and list user expense
 class ExpenseViewSet(viewsets.ModelViewSet):
     queryset = Expense.objects.all()
     serializer_class = ExpenseSerializer
@@ -30,5 +35,14 @@ class ExpenseViewSet(viewsets.ModelViewSet):
         for participant in expense.participants.all():
             writer.writerow([participant.user.name, participant.amount, participant.percentage])
         return response
+    
+# API view to retrieve individual user expenses
+class UserExpenseListView(generics.ListAPIView):
+    serializer_class = ExpenseSerializer  # Serializer to use for this view
+
+    # Filtering expenses by user
+    def get_queryset(self):
+        user_id = self.kwargs['user_id']  # Extracting user_id from URL
+        return Expense.objects.filter(created_by_id=user_id)  # Filtering expenses by created_by field
 
     
