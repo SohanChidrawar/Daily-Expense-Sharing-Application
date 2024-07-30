eaa8e477cf3576e2dd73e747e3bacfd068987ed4
Sohan Chidrawar <99244757+SohanChidrawar@users.noreply.github.com>
2024-07-30 14:46:51 +0530

    Update urls.py

--- expenses/urls.py
+++ expenses/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import UserViewSet, ExpenseViewSet
+from .views import UserViewSet, ExpenseViewSet, UserDetailView, UserExpenseListView
 
 router = DefaultRouter()
 router.register(r'user', UserViewSet, basename='user')
@@ -8,5 +8,7 @@ router.register(r'expenses', ExpenseViewSet, basename='expense')
 
 urlpatterns = [
     path('', include(router.urls)),
+    path('users/', UserDetailView.as_view(), name='user-list'),
+    path('expenses/', UserExpenseListView.as_view(), name='expense-list'),
     path('expenses/<int:pk>/download_balance_sheet/', ExpenseViewSet.as_view({'get': 'download_balance_sheet'})),
 ]
